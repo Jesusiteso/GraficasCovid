51187552c27cdbec35bf9cce158023b9b2cf0914
JesusVal <kzkz98@outlook.com>
2020-07-04 21:44:10 -0500

    Estadisticas object created and linked with charts prototypes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Estadisticas/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Estadisticas from './components/Estadisticas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Estadisticas
   }
 }
 </script>
---
+++ src/components/Estadisticas.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <H1>Hello from Estadisticas</H1>
+
+    <table >
+        <tr>
+            <td><LineChart :chart-data="datacollection" class="chart-container"></LineChart></td>
+            <td><PieChart :chart-data='datacollection' class="chart-container"></PieChart></td>
+        </tr>
+        <tr>
+            <td><VerticalBar :chart-data='datacollection' class="chart-container"></VerticalBar></td>
+            <td><HorizontalBar :chart-data='datacollection' class="chart-container"></HorizontalBar></td>
+        </tr>
+    </table>
+    
+</div>
+</template>
+
+<script>
+// import axios from 'axios';
+import LineChart from './lineChart.js';
+import PieChart from './pieChart.js';
+import HorizontalBar from './horizontalBar.js';
+import VerticalBar from './verticalBar.js';
+
+export default {
+    name: 'Estadisticas',
+    components: {LineChart, PieChart, HorizontalBar, VerticalBar},
+    data(){
+        return{
+            datacollection: null
+        }
+    },
+    mounted(){
+        this.datacollection = {
+            labels: ['one', 'two', 'three', 'four', 'five'],
+            datasets: [{
+                label: 'LineChart',
+                data: [10,20,30,40,50]
+            }]
+        }
+    }
+}
+</script>
+
+<style>
+.chart-container{
+    height: 200px;
+    width: 200px;
+}
+table{
+    height: 100px;
+    width: 100px;
+    border: 1px solid black;
+}
+</style>
